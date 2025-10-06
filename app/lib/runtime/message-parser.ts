4c786a8958aa0b44e96ac5ee215bab70b96598cd
Gerome-Elassaad <repo-booster@proton.me>
2025-10-06 11:45:42 +1100

    update

--- app/lib/runtime/message-parser.ts
+++ app/lib/runtime/message-parser.ts
@@ -388,7 +388,7 @@ export class StreamingMessageParser {
 
 const createArtifactElement: ElementFactory = (props) => {
   const elementProps = [
-    'class="__boltArtifact__"',
+    'class="__codinitArtifact__"',
     ...Object.entries(props).map(([key, value]) => {
       return `data-${camelToDashCase(key)}=${JSON.stringify(value)}`;
     }),
