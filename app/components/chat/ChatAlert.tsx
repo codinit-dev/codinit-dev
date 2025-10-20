430cb4243259a0f21ba99fa0fa78e76be9dcad5f
Gerome-Elassaad <repo-booster@proton.me>
2025-10-20 12:21:44 +1100

    default chat alerts fixed

--- app/components/chat/ChatAlert.tsx
+++ app/components/chat/ChatAlert.tsx
@@ -84,7 +84,7 @@ export default function ChatAlert({ alert, clearAlert, postMessage }: Props) {
                   )}
                 >
                   <div className="i-ph:chat-circle-duotone"></div>
-                  Ask codinit
+                  Ask CodinIT
                 </button>
                 <button
                   onClick={clearAlert}
