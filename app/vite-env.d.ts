2fb413ca11c66659465c4a9e7cb625abf7396f45
Gerome-Elassaad <repo-booster@proton.me>
2025-10-24 08:24:46 +1100

    fixed tsconfig

--- app/vite-env.d.ts
+++ app/vite-env.d.ts
@@ -1,9 +1,2 @@
 declare const __COMMIT_HASH: string;
 declare const __APP_VERSION: string;
-
-declare module '*.svg' {
-  import React = require('react');
-  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const src: string;
-  export default src;
-}
