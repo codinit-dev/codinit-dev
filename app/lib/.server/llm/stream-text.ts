1daae6bc02717e65ec7a7fa78cc1ac8c9942cb88
Gerome-Elassaad <repo-booster@proton.me>
2025-11-01 20:18:21 +1100

    update thought class names in server llm utilities

--- app/lib/.server/llm/stream-text.ts
+++ app/lib/.server/llm/stream-text.ts
@@ -60,7 +60,7 @@ export async function streamText(props: {
       return { ...message, content };
     } else if (message.role == 'assistant') {
       let content = message.content;
-      content = content.replace(/<div class=\\"__boltThought__\\">.*?<\/div>/s, '');
+      content = content.replace(/<div class=\\"__codinitThought__\\">.*?<\/div>/s, '');
       content = content.replace(/<div class=\\"__codinitThinking__\\"[^>]*>.*?<\/div>/gs, '');
       content = content.replace(/<think>.*?<\/think>/s, '');
       content = content.replace(/<codinitThinking[^>]*>.*?<\/codinitThinking>/gs, '');
