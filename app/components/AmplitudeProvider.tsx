f580ee78a480683c8219d672c7bfd2476fb2375d
Gerome-Elassaad <repo-booster@proton.me>
2025-11-13 23:47:08 +1100

    updated integrations

--- app/components/AmplitudeProvider.tsx
+++ app/components/AmplitudeProvider.tsx
@@ -4,10 +4,8 @@ import { sessionReplayPlugin } from '@amplitude/plugin-session-replay-browser';
 
 export function AmplitudeProvider() {
   useEffect(() => {
-    // Add Session Replay plugin
     amplitude.add(sessionReplayPlugin({ sampleRate: 1 }));
 
-    // Initialize Amplitude
     amplitude.init('7d559bd95c2dfb5a17653a383f8f2e21', {
       autocapture: {
         elementInteractions: true,
