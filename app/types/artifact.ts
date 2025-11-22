09dc9e01bad014a42fb94ea2ebc97d9521e4782f
Gerome-Elassaad <repo-booster@proton.me>
2025-11-23 03:44:34 +1100

    Update artifact types

--- app/types/artifact.ts
+++ app/types/artifact.ts
@@ -1,10 +1,10 @@
-export interface ExampleArtifactData {
+export interface CodinitArtifactData {
   id: string;
   title: string;
   type?: string | undefined;
 }
 
-export interface ThinkingArtifactData extends ExampleArtifactData {
+export interface ThinkingArtifactData extends CodinitArtifactData {
   type: 'thinking';
   steps: string[];
   content: string;
