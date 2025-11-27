ea34d3b65921047e6729c7076bc6082383c35e8a
Gerome-Elassaad <repo-booster@proton.me>
2025-11-27 17:48:51 +1100

    render Clerk Waitlist component as modal overlay

--- app/routes/waitlist.tsx
+++ app/routes/waitlist.tsx
@@ -1,5 +1,18 @@
 import { Waitlist } from '@clerk/remix';
+import { useNavigate } from '@remix-run/react';
+import * as RadixDialog from '@radix-ui/react-dialog';
+import { Dialog } from '~/components/ui/Dialog';
 
 export default function WaitlistPage() {
-  return <Waitlist />;
+  const navigate = useNavigate();
+
+  return (
+    <RadixDialog.Root open={true} onOpenChange={() => navigate('/')}>
+      <Dialog showCloseButton={true} onClose={() => navigate('/')}>
+        <div className="p-6 relative z-10">
+          <Waitlist afterJoinWaitlistUrl="/" />
+        </div>
+      </Dialog>
+    </RadixDialog.Root>
+  );
 }
