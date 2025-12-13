da1cb8dbc391c8d39996c2f773a6f6459497376b
Gerome-Elassaad <repo-booster@proton.me>
2025-12-13 22:47:17 +1100

    Revert v3_singleFetch flag from vite-electron.config.ts

--- vite-electron.config.ts
+++ vite-electron.config.ts
@@ -46,7 +46,6 @@ export default defineConfig((config) => {
           v3_relativeSplatPath: true,
           v3_throwAbortReason: true,
           v3_lazyRouteDiscovery: true,
-          v3_singleFetch: true,
         },
         serverModuleFormat: 'esm',
       }),
