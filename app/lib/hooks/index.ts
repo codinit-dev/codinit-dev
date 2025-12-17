44c61c0bfca9f9864ee03d6f3ad3160ce608eb57
Gerome-Elassaad <repo-booster@proton.me>
2025-12-18 03:08:38 +1100

    delete mock features hook and API files

--- app/lib/hooks/index.ts
+++ app/lib/hooks/index.ts
@@ -5,7 +5,6 @@ export * from './StickToBottom';
 export * from './useEditChatDescription';
 export { default } from './useViewport';
 export { useUpdateCheck } from './useUpdateCheck';
-export { useFeatures } from './useFeatures';
 export { useNotifications } from './useNotifications';
 export { useConnectionStatus } from './useConnectionStatus';
 export { useDebugStatus } from './useDebugStatus';
