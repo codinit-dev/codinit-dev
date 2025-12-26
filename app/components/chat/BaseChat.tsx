6fd82712fec7dd63d08a922ebca15f5cca44214f
Gerome-Elassaad <repo-booster@proton.me>
2025-12-27 02:40:36 +1100

    update BaseChat with new depth tokens

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -691,7 +691,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
             )}
 
             <StickToBottom
-              className={classNames('pt-6 px-2 sm:px-6 relative', {
+              className={classNames('pt-2 px-2 sm:px-6 relative', {
                 'h-full flex flex-col modern-scrollbar': chatStarted,
               })}
               resize="smooth"
