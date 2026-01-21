e909b007f989eed3037f44109941dfdb1900da1b
Gerome-Elassaad <repo-booster@proton.me>
2026-01-21 15:43:14 +1100

    Update NetlifyDeploy.client.tsx

--- app/components/deploy/NetlifyDeploy.client.tsx
+++ app/components/deploy/NetlifyDeploy.client.tsx
@@ -45,8 +45,6 @@ export function useNetlifyDeploy() {
         type: 'standalone',
       });
 
-      const deployArtifact = workbenchStore.artifacts.get()[deploymentId];
-
       // Notify that build is starting
       alertsStore.setDeployAlert({
         type: 'info',
