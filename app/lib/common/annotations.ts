9bb28efd0a6bee0911308bb56a402847a280860a
Gerome-Elassaad <repo-booster@proton.me>
2026-01-21 15:27:36 +1100

    Update annotations.ts

--- app/lib/common/annotations.ts
+++ app/lib/common/annotations.ts
@@ -1,18 +1,17 @@
-
-export const REPEATED_ERROR_REASON = "repeated-error";
+export const REPEATED_ERROR_REASON = 'repeated-error';
 
 export type Usage = {
-    completionTokens: number;
-    promptTokens: number;
-    totalTokens: number;
-    xaiCachedPromptTokens: number;
-    openaiCachedPromptTokens: number;
-    anthropicCacheReadInputTokens: number;
-    anthropicCacheCreationInputTokens: number;
-    googleCachedContentTokenCount: number;
-    googleThoughtsTokenCount: number;
-    bedrockCacheWriteInputTokens: number;
-    bedrockCacheReadInputTokens: number;
-    toolCallId?: string;
-    providerMetadata?: any;
+  completionTokens: number;
+  promptTokens: number;
+  totalTokens: number;
+  xaiCachedPromptTokens: number;
+  openaiCachedPromptTokens: number;
+  anthropicCacheReadInputTokens: number;
+  anthropicCacheCreationInputTokens: number;
+  googleCachedContentTokenCount: number;
+  googleThoughtsTokenCount: number;
+  bedrockCacheWriteInputTokens: number;
+  bedrockCacheReadInputTokens: number;
+  toolCallId?: string;
+  providerMetadata?: any;
 };
